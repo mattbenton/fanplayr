2539c864095a5fd320b516d014797fed5806a5e2
mattbenton <matt@mattbenton.net>
2012-09-21 12:59:30 -0700

    Added safeguards to prevent clearing production cache

--- bookmarklets/reset-cache.js
+++ bookmarklets/reset-cache.js
@@ -1,3 +1,14 @@
-(function() {
-	console.log(document.location.host + '/test.reset/');
-})();
+(function(d, s) {
+	var a = d.createElement(s),
+		host = d.location.host;
+
+	if ( host.match(/my.fanplayr.com/) ) {
+		alert('Cannot reset cache in production environments!');
+	} else if ( host.match(/(site.fanplayr|stage.fanplayr.com)/) ) {
+		a.async = true;
+		a.src = '//' + d.location.host + '/test.reset/';
+		d.body.appendChild(a);
+	} else {
+		alert('Cannot reset cache. This page isn\'t a recognised testing domain');
+	}
+})(document, 'script')
